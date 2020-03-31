77b4ad68b12535cd57c525612e70bc0f6b3785f0
zhiyuandaoren <czw3106627097@163.com>
2020-03-31 14:26:03 +0800

    update

---
+++ 新建文件夹/选择排序法.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void sort (int array[],int n);
+int main()
+{
+	int a[10],i;
+	printf("enter array :\n");
+	for(i=0;i<10;i++) scanf("%d",&a[i]);
+	sort(a,10);
+	printf("The sorted array :\n");
+	for(i=0;i<10;i++) printf("%d\t",a[i]);
+	printf("\n");
+	return 0;
+}
+void sort (int array[],int n)
+{
+	int i,j,k,t;
+	for(i=0;i<n-1;i++)
+	{
+		k=i;
+			for(j=i+1;j<n;j++)
+				if(array[j]<array[k]) k=j;
+		t=array[k];
+		array[k]=array[i];
+		array[i]=t;
+	}
+}
